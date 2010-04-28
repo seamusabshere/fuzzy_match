c85fbd4434a66c8ef7188ba78be0f688e9d9a215
Seamus Abshere <seamus@abshere.net>
2010-04-27 21:04:53 -0400

    Update icao-bts script example.

--- examples/icao-bts.rb
+++ examples/icao-bts.rb
@@ -9,37 +9,48 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'loose_t
 $logger = Logger.new STDERR
 $logger.level = Logger::DEBUG
 $logger.datetime_format = "%H:%M:%S"
-# $tee = File.open('tee.csv', 'w')
-$tee = STDOUT
+$tee = File.open('tee.csv', 'w')
+# $tee = STDOUT
 
-# $ltd_left = /corp 36 bonanza/i
+# $ltd_left = /(super|bonanza)/i
 # $ltd_right = /bonanza d-35/i
-# $ltd_inside_left = /bonanza/i
-# $ltd_inside_right = /king air/i
-# $ltd_inside_left_not = /allison/i
-# $ltd_inside_print = true
+# $ltd_dd_left = /bonanza/i
+# $ltd_dd_right = /musk/i
+# $ltd_dd_left_not = /allison/i
+# $ltd_dd_print = true
+# $ltd_ddd_left = /bonanza/i
+# $ltd_ddd_right = /musk/i
+# $ltd_ddd_left_not = /allison/i
+# $ltd_ddd_print = true
 
-right = RemoteTable.new :url => 'http://www.bts.gov/programs/airline_information/accounting_and_reporting_directives/csv/number_260.csv',
+@right = RemoteTable.new :url => 'http://www.bts.gov/programs/airline_information/accounting_and_reporting_directives/csv/number_260.csv',
                         :select => lambda { |row| row['Aircraft Type'].to_i.between?(1, 998) and row['Manufacturer'].present? }
 
-tightenings = RemoteTable.new :url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=0&output=csv', :headers => false
+@tightenings = RemoteTable.new :url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=0&output=csv', :headers => false
 
-restrictions = RemoteTable.new :url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=3&output=csv', :headers => false
+@restrictions = RemoteTable.new :url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=3&output=csv', :headers => false
 
-positives = RemoteTable.new :url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=1&output=csv', :headers => false
+@blockings = RemoteTable.new :url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=4&output=csv', :headers => false
 
-negatives = RemoteTable.new :url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=2&output=csv', :headers => false
+@positives = RemoteTable.new :url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=1&output=csv', :headers => false
 
+@negatives = RemoteTable.new :url => 'http://spreadsheets.google.com/pub?key=tiS_6CCDDM_drNphpYwE_iw&single=true&gid=2&output=csv', :headers => false
 
-# ('A'..'Z').each do |letter|
-%w{ B }.each do |letter|
-  left = RemoteTable.new :url => "http://www.faa.gov/air_traffic/publications/atpubs/CNT/5-2-#{letter}.htm",
+%w{ tightenings restrictions blockings }.each do |name|
+  $logger.info name
+  $logger.info "\n" + instance_variable_get("@#{name}").rows.map { |row| row[0] }.join("\n")
+  $logger.info "\n"
+end
+
+('A'..'Z').each do |letter|
+# %w{ E }.each do |letter|
+  @left = RemoteTable.new :url => "http://www.faa.gov/air_traffic/publications/atpubs/CNT/5-2-#{letter}.htm",
                          :encoding => 'US-ASCII',
                          :row_xpath => '//table/tr[2]/td/table/tr',
                          :column_xpath => 'td'
 
-  d = LooseTightDictionary.new left, right, tightenings, restrictions, :logger => $logger, :tee => $tee
+  d = LooseTightDictionary.new @left, @right, @tightenings, @restrictions, @blockings, :logger => $logger, :tee => $tee
   d.left_reader = lambda { |row| row['Manufacturer'] + ' ' + row['Model'] }
   d.right_reader = lambda { |row| row['Manufacturer'] + ' ' + row['Long Name'] }
-  d.check positives, negatives
+  d.check @positives, @negatives
 end
