c8e6a3bdcfbceba774996e6fcc24ab708ebf9c78
Seamus Abshere <seamus@abshere.net>
2011-04-05 08:31:54 -0500

    convert test helper to bundler-friendly one

--- test/helper.rb
+++ test/helper.rb
@@ -1,12 +1,12 @@
 require 'rubygems'
+require 'bundler'
+Bundler.setup
 require 'test/unit'
 require 'shoulda'
 require 'logger'
-# require 'ruby-debug'
-
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib', 'loose_tight_dictionary'))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'loose_tight_dictionary'
 
 class Test::Unit::TestCase
 end
