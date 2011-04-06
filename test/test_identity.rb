a3f7114059d79b931666561229cae37e9de87927
Seamus Abshere <seamus@abshere.net>
2011-04-06 14:57:27 -0500

    give ExtractRegexp its own test

--- test/test_identity.rb
+++ test/test_identity.rb
@@ -1,27 +1,12 @@
 require 'helper'
 
 class TestIdentity < Test::Unit::TestCase
-  def test_001_regexp
-    i = LooseTightDictionary::Identity.new %r{\A\\?/(.*)etc/mysql\$$}
-    assert_equal %r{\A\\?/(.*)etc/mysql\$$}, i.regexp
-  end
-  
-  def test_002_regexp_from_string
-    i = LooseTightDictionary::Identity.new '%r{\A\\\?/(.*)etc/mysql\$$}'
-    assert_equal %r{\A\\?/(.*)etc/mysql\$$}, i.regexp
-  end
-  
-  def test_003_regexp_from_string_using_slash_delim
-    i = LooseTightDictionary::Identity.new '/\A\\\?\/(.*)etc\/mysql\$$/'
-    assert_equal %r{\A\\?/(.*)etc/mysql\$$}, i.regexp
-  end
-  
-  def test_004_allow
+  def test_001_allow
     i = LooseTightDictionary::Identity.new %r{(A)[ ]*(\d)}
     assert i.allow?('A1', 'A     1foobar')
   end
   
-  def test_005_disallow
+  def test_002_disallow
     i = LooseTightDictionary::Identity.new %r{(A)[ ]*(\d)}
     assert !i.allow?('A1', 'A     2foobar')    
   end
