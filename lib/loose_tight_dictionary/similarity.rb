0af34f40d992e20d7d86d39a9a2dc42771f7dde2
Seamus Abshere <seamus@abshere.net>
2011-10-20 21:17:45 -0500

    replace a venerable old ruby-forum method with a native one

--- lib/loose_tight_dictionary/similarity.rb
+++ lib/loose_tight_dictionary/similarity.rb
@@ -34,7 +34,7 @@ class LooseTightDictionary
     end
     
     def best_variants
-      @best_variants ||= cart_prod(wrapper1.variants, wrapper2.variants).sort do |tuple1, tuple2|
+      @best_variants ||= wrapper1.variants.product(wrapper2.variants).sort do |tuple1, tuple2|
         wrapper1_variant1, wrapper2_variant1 = tuple1
         wrapper1_variant2, wrapper2_variant2 = tuple2
 
@@ -47,16 +47,6 @@ class LooseTightDictionary
     
     def inspect
       %{#<Similarity "#{wrapper2.to_str}"=>"#{best_wrapper2_variant}" versus "#{wrapper1.to_str}"=>"#{best_wrapper1_variant}" weight=#{"%0.5f" % weight} best_score=#{"%0.5f" % best_score.to_f}>}
-    end
-    
-    # Thanks William James!
-    # http://www.ruby-forum.com/topic/95519#200484
-    def cart_prod(*args)
-      args.inject([[]]){|old,lst|
-        new = []
-        lst.each{|e| new += old.map{|c| c.dup << e }}
-        new
-      }
     end
   end
 end
